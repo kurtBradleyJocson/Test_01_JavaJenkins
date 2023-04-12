4e9ed8b85a55350a94b42be7451b49ad5e1bc3c3
Kurt Bradley Jocson <kurt.bradley.jocson@gmail.com>
2023-04-12 22:26:40 +0800

    3rd

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+		System.out.print("Enter Name: ");
+		
+		String name = scan.nextLine();
+		
+		System.out.println("Hello: " + name);
+		
 	}
 
 }
